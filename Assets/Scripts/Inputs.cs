be83e5a0b45195d5d5cc7941039d150a7b70189d
Sachin Acharya <sachina@mechmocha.com>
2016-10-14 11:41:00 +0530

    add plugins, and input script

---
+++ Assets/Scripts/Inputs.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+using UniRx;
+using UniRx.Triggers;
+
+public class Inputs : MonoBehaviour {
+
+	public IObservable<Vector2> Movement {get; private set;}
+	
+
+	// Use this for initialization
+	void Awake () {
+		Movement = this.FixedUpdateAsObservable ()
+			.Select(_ => {
+				var x = Input.GetAxis("Horizontal");
+				var y = Input.GetAxis("Vertical");
+				return new Vector2(x,y).normalized;
+			});
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
